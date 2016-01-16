24a4d92dad14b877ca5785884e9d22fb2c74fddb
kage-mac <kagemiku@gmail.com>
2016-01-16 20:47:37 +0900

    Add setting files for Travis-CI.

---
+++ .travis_build.sh
@@ -0,0 +1,20 @@
+echo "Generating googletest libraly"
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -isystem tests/googletest/googletest/ -c -o tests/gtest-all.o tests/googletest/googletest/src/gtest-all.cc
+echo "Done compiling gtest-all.cc"
+ar -rv tests/libgtest.a tests/gtest-all.o
+echo "Done archiving gtest-all.o"
+echo "Compiling sources..."
+clang++ -std=c++1y -pthread -g -Wall -Wextra  -c -o tests/mat_generator.o sources/mat_generator.hpp
+echo "Done compiling kmosaic.cpp"
+echo "Compiling unit tests..."
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -c -o tests/main.o tests/main.cpp
+echo "Done compiling main.cpp"
+clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include  -c -o tests/mat_generator_test.o tests/mat_generator_test.cpp
+echo "Done compiling kmosaic_test.cpp"
+clang++ -std=c++1y -g -Wall -Wextra -o tests/gtest tests/mat_generator.o tests/main.o tests/mat_generator_test.o -pthread -Ltests/ -lgtest -lopencv_core
+echo "Running unit tests..."
+tests/gtest -v
+result=$?
+rm -r tests/gtest tests/mat_generator.o tests/main.o tests/mat_generator_test.o tests/gtest-all.o tests/libgtest.a
+echo "Unit tests completed : $result"
+exit $result
