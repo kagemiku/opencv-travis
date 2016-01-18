c6deeab4f0f4475ccdc3b0d3fbf9dd3ea1a267cf
kage-mac <kagemiku@gmail.com>
2016-01-18 18:50:38 +0900

    Use OSX system on Travis-CI.

--- .travis_build.sh
+++ .travis_build.sh
@@ -4,14 +4,14 @@ echo "Done compiling gtest-all.cc"
 ar -rv tests/libgtest.a tests/gtest-all.o
 echo "Done archiving gtest-all.o"
 echo "Compiling sources..."
-clang++ -std=c++1y -pthread -g -Wall -Wextra  -c -o tests/mat_generator.o sources/mat_generator.hpp
+clang++ -std=c++1y -pthread -g -Wall -Wextra  -I/usr/local/include -c -o tests/mat_generator.o sources/mat_generator.hpp
 echo "Done compiling kmosaic.cpp"
 echo "Compiling unit tests..."
 clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include -c -o tests/main.o tests/main.cpp
 echo "Done compiling main.cpp"
-clang++ -std=c++1y -pthread -g -Wall -Wextra -isystem tests/googletest/googletest/include  -c -o tests/mat_generator_test.o tests/mat_generator_test.cpp
+clang++ -std=c++1y -pthread -g -Wall -Wextra -I/usr/local/include -isystem tests/googletest/googletest/include  -c -o tests/mat_generator_test.o tests/mat_generator_test.cpp
 echo "Done compiling kmosaic_test.cpp"
-clang++ -std=c++1y -g -Wall -Wextra -o tests/gtest tests/mat_generator.o tests/main.o tests/mat_generator_test.o -pthread -Ltests/ -lgtest -lopencv_core
+clang++ -std=c++1y -g -Wall -Wextra -o tests/gtest tests/mat_generator.o tests/main.o tests/mat_generator_test.o -pthread -Ltests/ -L/usr/local/lib  -lgtest -lopencv_core
 echo "Running unit tests..."
 tests/gtest -v
 result=$?
